24bf27de1a8a53e4d24cb63b0d137cef267dcdb4
Auxence <auxence.blondel@gmail.com>
2020-03-20 00:26:09 +0100

    correction logo src

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ const darkMod = document.getElementById("darkMode");
 const logoCabreta = document.getElementById("logo");
 const rootElt = document.documentElement;
 
-darkMod.onclick = function(){
+logoCabreta.src = "images/logo/CabretaLogoBlack1.png"
 
+darkMod.onclick = function(){
+    
     if(rootElt.getAttribute('data-theme') === 'dark'){  // White mode
         rootElt.setAttribute('data-theme', 'light');
         logoCabreta.src = "images/logo/CabretaLogoBlack1.png";
